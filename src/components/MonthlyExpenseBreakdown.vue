37f01ce04da2655b74074301bf8ace8604548936
Braxton Shoop <braxton.shoop@gmail.com>
2021-03-29 12:23:15 -0500

    initial styling complete

---
+++ src/components/MonthlyExpenseBreakdown.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="monthlyExpenseBreakdownContainer">
+    <div class="monthlyExpenseBreakdownLabel">Monthly Expense Breakdown</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.monthlyExpenseBreakdownContainer {
+  width: 55em;
+  height: 30em;
+  background-color: white;
+
+  border: 3px;
+  border-color: purple;
+  border-style: solid;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: column;
+}
+
+.monthlyExpenseBreakdownLabel {
+  font-size: 20pt;
+  color: green;
+  font-weight: 800;
+  margin-right: 18em;
+  margin-bottom: 15em;
+}
+</style>
